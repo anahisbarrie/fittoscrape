dc1d4e3697f4606015dd2a2bcb9970e3eebef510
anahisbarrie <anahis.barrie@gmail.com>
2019-09-04 21:29:48 -0400

    mogno

--- server.js
+++ server.js
@@ -19,9 +19,9 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database/ /
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
+mongoose.connect(MONGODB, { useNewUrlParser: true })
 
 //mongoose.connect('mongodb://user:password@sample.com:port/dbname', { useNewUrlParser: true })
 
